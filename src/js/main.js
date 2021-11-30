5d89192a1de5d469c183e9e3491845a13fc5cb86
sniezhokrm <sniezhokrm@gmail.com>
2021-11-30 11:39:08 +0200

    add play video from YouTube on first page, with }

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 import Slider from './modules/slider';
+import VideoPlayer from './modules/playVideo';
 
-window.document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', () => {
+    const slider = new Slider('.page', '.next');
+    slider.render();
 
-  const slider = new Slider('.page', '.next', '.hanson');
-
-  slider.render();
-
-
+    const player = new VideoPlayer('.showup .play', '.overlay');
+    player.init();
 });
